f50b00f34e10792c4a9e7e7857da88c2e978f9e0
kongdong <kongdong@repeatlink.cn>
2019-02-12 15:26:33 +0800

    init rui lib

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import RInput from "./components/RInput"; 
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+         <RInput/>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/RInput/index.jsx
@@ -0,0 +1,5 @@
+import React,{ Component } from "react" ;
+export const RInput = ()=>{
+    return <input type="text"/>
+}
+export default RInput ;
